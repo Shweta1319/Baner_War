db48d8a1f9829b29aae70a39be8cc78cbca30d86
Shweta1319 <47740471+Shweta1319@users.noreply.github.com>
2020-01-18 20:27:42 +0530

    Updating DeploytoTomcat stage

--- Jenkinsfile.sh
+++ Jenkinsfile.sh
@@ -15,17 +15,10 @@ pipeline {
                         sh 'mvn install'
                     }
            }
-
-
-            stage('DeploytoNexus'){
-                    steps{
-                      sh 'mvn deploy'
-                    }
-             }
              
              stage('DeploytoTomcat'){
                     steps{
-                        deploy adapters: [tomcat8(credentialsId: '1de8381f-0df6-4c36-99af-467ac731a2a7', path: '', url: 'http://18.222.185.192:9090/')], contextPath: 'banerwar', war: '**/*.war'                    }
+                        deploy adapters: [tomcat8(credentialsId: '1de8381f-0df6-4c36-99af-467ac731a2a7', path: '', url: 'http://54.80.214.39:8080/')], contextPath: 'banerwar', war: '**/*.war'                    }
              }
 
     }
